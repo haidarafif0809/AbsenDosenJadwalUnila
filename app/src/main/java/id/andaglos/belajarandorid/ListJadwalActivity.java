f5eb0541e227eb93081bdc6b6ca39d598ec83345
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-10-07 14:30:31 +0700

    Import uri di list jadwalan

--- app/src/main/java/id/andaglos/belajarandorid/ListJadwalActivity.java
+++ app/src/main/java/id/andaglos/belajarandorid/ListJadwalActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.MenuItemCompat;
